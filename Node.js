99f5385aa2ff9529f5dd9b8d071a77f605bfa45f
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-01-24 20:03:30 +0530

    create http request
    create http request

--- Node.js
+++ Node.js
@@ -1,4 +1,13 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-const fs = require('fs');
-fs.writeFileSync('hello.txt','Hello from Node.js')
+// const fs = require('fs');
+// fs.writeFileSync('hello.txt','Hello from Node.js')
+
+const http = require('http');
+const server = http.createServer((request,response)=>{
+    console.log(request);
+    console.log('ayush')
+
+});
+
+server.listen(4000);
